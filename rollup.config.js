2157e8b40a3d403077142a2786ca5465a5b17db4
rootgog <dev.georgebishop@gmail.com>
2019-10-16 13:17:19 +0100

    Added build script and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
+import {
+    terser
+} from "rollup-plugin-terser";
+
 export default {
     input: 'src/router.js',
-    output: {
-        file: 'lib/router.js',
-        format: 'umd',
-        name: "Router.js"
-    }
+    output: [{
+            file: 'lib/router.min.js',
+            format: 'iife',
+            name: "Router"
+        },
+        {
+            file: 'lib/router.js',
+            format: 'cjs',
+        }
+    ],
+    plugins: [
+        terser({
+            include: [/^.+\.min\.js$/]
+        })
+    ]
 };
